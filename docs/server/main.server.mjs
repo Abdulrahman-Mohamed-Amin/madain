834fe7c7aaf03065cbf90ebd5338dfb26ec8cb81
aboamin <eeaarrssnn40@gmail.com>
2025-10-17 17:07:31 +0300

    twlve

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-MGRPN3KX.mjs";import"./chunk-R65YLAAR.mjs";import"./chunk-4IEFNZBC.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-VSPURP7D.mjs";import"./chunk-R65YLAAR.mjs";import"./chunk-4IEFNZBC.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
