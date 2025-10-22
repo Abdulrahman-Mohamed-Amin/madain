e58dbf180c539388a9480c617f721ed5a0729983
aboamin <eeaarrssnn40@gmail.com>
2025-10-22 16:30:36 +0300

    17th

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-K7FWIO4R.mjs";import"./chunk-WP247MS4.mjs";import"./chunk-ZXXHPUZ6.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-S3RSHPYI.mjs";import"./chunk-WP247MS4.mjs";import"./chunk-ZXXHPUZ6.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/featured/bayot/bayot.component.ts
+++ src/app/featured/bayot/bayot.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-bayot',
@@ -8,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrl: './bayot.component.css'
 })
 export class BayotComponent {
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  
+    ngAfterViewInit(): void {
+      if (!isPlatformBrowser(this.platformId)) return;
+      const swiper = new Swiper('.mySwiper2', {
+        slidesPerView:1 ,
+        loop: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          dynamicBullets: true,
+          dynamicMainBullets: 1,
+        },
+        autoplay: {
+          delay: 4000,
+        },
+        speed: 500,
+      });
+    }
 
 }
